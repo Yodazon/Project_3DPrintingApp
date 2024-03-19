c1125d7f45546e4e88f7f1ca40b10f4a3580db67
Yodazon <michael.karas96@gmail.com>
2024-03-19 17:55:31 -0400

    streamlit app work

---
+++ streamlit/preprocessImg.py
@@ -0,0 +1,20 @@
+import torchvision.transforms as transforms
+from PIL import Image
+
+
+image_size = 227
+
+def preProcess(uploaded_image):
+        # Load and preprocess the image
+    image = Image.open(uploaded_image).convert('RGB')
+
+    transform = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.CenterCrop(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    input_image = transform(image).unsqueeze(0)
+
+    return input_image
---
+++ streamlit/streamlit_app.py
@@ -0,0 +1,10 @@
+### Code to develop a streamlit app
+### Using the model I made in CNN for 3D prints
+import streamlit as st
+import preprocessImg as pImg
+
+st.write("Hi there! Welcome to the 3D Print App, where we try to solve your 3D Printing issues")
+
+uploaded_image = st.file_uploader("Upload Image Here")
+
+processedImage = pImg.preProcess(uploaded_image)
