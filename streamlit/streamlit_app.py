40738c819a0e7c0aee82cd5ea48a10cae89fb58e
Yodazon <michael.karas96@gmail.com>
2024-03-19 18:39:09 -0400

    Uploading model to streamlit app

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -2,19 +2,49 @@
 ### Using the model I made in CNN for 3D prints
 import streamlit as st
 import preprocessImg as pImg
+import torch
+import torchvision.models as models
+import pyTorchModel
+
+st.title("3D Printing Issue Solver")
+
+def predictImage(processedImage):
+    class_names = {0: 'good', 1: 'spaghetti', 2: 'stringing', 3: 'underextrusion'}
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+
+    model = pyTorchModel()
+    model_weights_path = "CNNBuilding\models_in_folder\CNNModelV0_2.pth"
+    model.load_state_dict(torch.load(model_weights_path))
+
+    model.eval()
+
+    with torch.no_grad():
+        input_image = processedImage.to(device)
+        output = model(input_image)
+    
+    predicted_index = output.argmax(1).item()
+    predicted_class_name = class_names[predicted_index]
+    
+    return predicted_class_name
+
+
 
 st.write("Hi there! Welcome to the 3D Print App, where we try to solve your 3D Printing issues")
 
 uploaded_image = st.file_uploader("Upload Image Here")
 
-
 if uploaded_image is not None:
     st.write("Image you have uploaded is below:")
     st.image(uploaded_image)
-    #processedImage = pImg.preProcess(uploaded_image)
+    processedImage = pImg.preProcess(uploaded_image)
+
+    prediction = predictImage(processedImage)
+    st.write(f"The predicted problem is {prediction} ")
 else:
     st.write("No image yet")
-
-
-
-
